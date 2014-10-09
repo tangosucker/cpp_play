0c6c259b0d57011bccd2e01cecce4f954f56bdf2
tangosucker <real.r197@gmail.com>
2014-10-09 18:35:04 +0300

    commit test over netbeans

---
+++ netbeans_test/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: tango
+ *
+ * Created on neljapäev, 9. oktoober 2014. a, 18:09
+ */
+
+#include <iostream>
+#include <string>
+
+
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    std::string t("test2");
+    std::cout << t << std::endl;
+    return 0;
+}
+
